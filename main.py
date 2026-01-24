ac826e893ec50acda92643fdc815ed6d806f4ea4
Lewis <lewisstarkov@gmail.com>
2026-01-24 21:50:26 +0400

    disable access logs

--- main.py
+++ main.py
@@ -184,4 +184,4 @@ async def check_usernames(req: CheckRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, access_log=False)
